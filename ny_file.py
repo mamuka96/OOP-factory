8bc2f8d53fe26d73508bb49a5befce968ac75a84
mshenheliia <mamuka.shenheliia@cleverbridge.com>
2024-11-05 16:11:46 +0200

    add decorator

--- ny_file.py
+++ ny_file.py
@@ -1,5 +1,13 @@
 
+def devide(func):
+    def wrapper(a,b):
+        res = func(a,b) / 2
+        return res
+    return wrapper
 
-
+@devide
 def add_num(a,b):
     return a + b
+
+
+print(add_num(5,5))
